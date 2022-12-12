abd89fbee2b68f1fb3c3615a9a863ff84c91f58e
Janis Zisenis <janis.zisenis@netlight.com>
2022-12-12 21:44:54 +0100

    step 3: change function declaration (rename perf parameter)

--- Java/TheaterBilling/src/main/java/org/example/StatementBuilder.java
+++ Java/TheaterBilling/src/main/java/org/example/StatementBuilder.java
@@ -29,22 +29,22 @@ public class StatementBuilder {
         return result;
     }
 
-    public static int amountFor(Performance perf, Play play) {
+    public static int amountFor(Performance performance, Play play) {
         var result = 0;
 
         switch(play.type) {
             case "tragedy":
                 result = 40000;
-                if(perf.audience > 30) {
-                    result += 1000 * (perf.audience -30);
+                if(performance.audience > 30) {
+                    result += 1000 * (performance.audience -30);
                 }
                 break;
             case "comedy":
                 result = 30000;
-                if(perf.audience > 20) {
-                    result += 10000 + 500 * (perf.audience - 20);
+                if(performance.audience > 20) {
+                    result += 10000 + 500 * (performance.audience - 20);
                 }
-                result += 300 * perf.audience;
+                result += 300 * performance.audience;
                 break;
             default:
                 throw new RuntimeException("unknown type: " + play.type);
