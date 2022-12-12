d1d206da0faf09e05aa9352aff9c0893cd8ac1ba
Dario Casadevall <dario.casadevall@beyonnex.io>
2022-12-12 21:12:56 +0100

    fix test and remove cashed js files

--- typescript/theatre-billing/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var statementBuilder_1 = require("./statementBuilder");
-var plays_json_1 = require("./plays.json");
-var invoices_json_1 = require("./invoices.json");
-var state = (0, statementBuilder_1.statement)(invoices_json_1["default"], plays_json_1["default"]);
-console.log(state);
--- typescript/theatre-billing/index.test.ts
+++ typescript/theatre-billing/index.test.ts
@@ -10,9 +10,9 @@ describe('testing statement function', () => {
         let expected =
             "Statement for BigCo\n" +
             " Hamlet: $650.00 (55 seats)\n" +
-            " As You Like It: $490.00 (35 seats)\n" +
+            " As You Like It: $580.00 (35 seats)\n" +
             " Othello: $500.00 (40 seats)\n" +
-            "Amount owed is $1,640.00\n" +
+            "Amount owed is $1,730.00\n" +
             "You earned 47 credits\n"
         expect(expected).toBe(result)
     })
